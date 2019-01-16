4614e71310d443e4bba1893a4a326b84ea625ccf
Champy13 <hgftdrd@hotmail.com>
2019-01-16 13:34:36 -0600

    Funciones encode y decode funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -4,8 +4,8 @@ window.cipher = {
     let str_encode = '';
 
     for (let i = 0; i < string.length; i++) {
-      let charac_ascci = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
-      let charac_encode = String.fromCharCode(charac_ascci);
+      let charac_ascii = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+      let charac_encode = String.fromCharCode(charac_ascii);
       str_encode += charac_encode;
     }
     return str_encode;
@@ -16,8 +16,8 @@ window.cipher = {
     let str_decode = '';
 
     for (let i = 0; i < string.length; i++) {
-      let charac_ascci = (string.charCodeAt(i) + 65 - offset) % 26 - 65;
-      let charac_decode = String.fromCharCode(charac_ascci);
+      let charac_ascii = (string.charCodeAt(i) + 65 - offset) % 26 + 65;
+      let charac_decode = String.fromCharCode(charac_ascii);
       str_decode += charac_decode;
     }
     return str_decode;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-document.getElementById("myBtn").addEventListener("click", myFunction);
+document.getElementById("btn_encode").addEventListener("click", encode_btn);
 
-function myFunction() {
-    const offset = parseInt(document.getElementById("offset").value);
-    const string = document.getElementById("str_encode").value;
-    document.getElementById("str_encode_result").innerHTML = window.cipher.encode(offset,string);
+function encode_btn() {
+    const offset_encode = parseInt(document.getElementById("offset").value);
+    const string_encode = document.getElementById("str_recive").value;
+    document.getElementById("str_result").innerHTML = window.cipher.encode(offset_encode,string_encode);
+}
+
+document.getElementById("btn_decode").addEventListener("click", decode_btn);
+
+function decode_btn() {
+    const offset_decode = parseInt(document.getElementById("offset").value);
+    const string_decode= document.getElementById("str_recive").value;
+    document.getElementById("str_result").innerHTML = window.cipher.decode(offset_decode,string_decode);
 }
 
 
